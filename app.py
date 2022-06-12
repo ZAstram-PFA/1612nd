0d2dce656de956c939f79ff46f6343b58e8477dd
ZAstram-PFA <93171714+ZAstram-PFA@users.noreply.github.com>
2022-06-12 13:18:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,25 @@
-from flask import Flask
-from data import products #takes the products dictionary from the data.py
+from flask import Flask, render_template
+from data import data
 app = Flask(__name__)
 
+
 @app.route('/')
-def index(): #homepage HTML code
-    return """<h1>Children's Corner!</h1>
-            <p>We offer a great selection of products for kids</p>
-            <p>Click on the links below!</p>
-            <ul>
-                <li><a href='/products/books'>Books for kids!</a></li>
-                <li><a href='/products/toys'>Educational toys!</a></li>
-                <li><a href='/products/school supplies'>School supplies!</a></li>
-            </ul>
-            <footer><a href='/about'>About us!</a></footer>
-            """
+def index():
+    return render_template('index.html')
 
-@app.route('/about') #About Us page HTML code, maybe use part of the documentation?
+@app.route('/about')
 def abtpage():
-    return "<h1>About Us!</h1>" #use """ """ for multiple lines in one quote string
+    return render_template('about.html')
 
-@app.route('/products/<pro_duct>') 
-def product(pro_duct): #This is like the animals function in the adopt-a-pet code
-    html = "<h1>List of {}</h1>".format(pro_duct)
-    html += "<ul>"
-    
-    for count, product in enumerate(products[pro_duct]): #I followed Step 16 in the project
-        html+="<li><a href='/products/"+pro_duct+"/"+str(count)+"'>"+product['name']+"</a></li>" #I formatted it like this because other formats wouldn't work lol
+@app.route('/product/<data_type>')
+def product(data_type): 
+    data_list = data[data_type]
+    return render_template('products.html',product_type_template=data_type, product_template=data_list)
 
-    
-    html+= "</ul>"
-    return html
-    
-@app.route('/products/<prod_type>/<int:x>') #prod_type is either books, toys or school supplies; x is the index of the specific item
-def prod(prod_type,x): #this is the pet function described in step 13
-    prod = products[prod_type][x] 
-    html = "<h1>{}</h1>".format(prod['name'])
-    
-    html += "<p>Creator/Manufacturer: {}</p>".format(prod['creator'])
-    html += "<p>Description: {}</p>".format(prod['description'])
-    html += "<p>Price: {}</p>".format(prod['price'])
-
-    
-    
-    
-    return html
+@app.route('/product/<data_type>/<int:data_id>') 
+def datas(data_type,data_id):
+    data_profile = data[data_type][data_id] 
+    return render_template('stuff.html',data_profile=data_profile)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- data.py
+++ data.py
@@ -1,35 +1,72 @@
-# still needs images??
-products = {
-    'books':[
+data = {
+    'books': [
         {
             'name': 'One Fish Two Fish Red Fish Blue Fish',
             'creator': 'Dr. Seuss',
             'price': '200.00 PHP',
-            'description':'A bestselling rhyming book by famous author Dr. Seuss!'
+            'description': 'A bestselling rhyming book by famous author Dr. Seuss!',
+            'url': 'https://englishwooks.com/2439-large_default/one-fish-two-fish-red-fish-blue-fish-dr-seuss-blue-back-book-.jpg'
         },
         {
             'name': 'The Very Hungry Caterpillar',
             'creator': 'Eric Carle',
             'price': '200.00 PHP',
-            'description': 'A very popular, adorably illustrated book about counting and food!'
+            'description': 'A very popular, adorably illustrated book about counting and food!',
+            'url': 'https://m.media-amazon.com/images/I/41w4B0f21VL._AC_SY1000_.jpg'
+        },
+        {
+            'name': 'Charlie and the Chocolate Factory',
+            'creator': 'Roald Dahl',
+            'price': '415.00 PHP',
+            'description': 'A novel by the beloved author Roald Dahl that got adapted into a hit movie!',
+            'url': 'https://upload.wikimedia.org/wikipedia/en/f/f6/Charlie_and_the_Chocolate_Factory_original_cover.jpg'
         }
     ],
-    'toys':[
+    'toys': [
         {
             'name': 'Abacus',
             'creator': 'Juan Dela Cruz',
-            'price': '500 PHP',
-            'description': 'A simple counting instrument!'
+            'price': '500.00 PHP',
+            'description': 'A simple counting instrument!',
+            'url': 'https://p.globalsources.com/IMAGES/PDT/S1148846240/2017-new-design-small-intelligent-wooden-abacus.jpg'
+        },
+        {
+            'name': 'LEGO Basic Building Block Sets',
+            'creator': 'The LEGO Group',
+            'price': '3,500.00 PHP',
+            'description': 'An iconic, classic toy, these sets of building blocks are perfect for stimulating creativity in young kids. Warning: Not for children under 4 years old!',
+            'url': 'https://m.media-amazon.com/images/I/91fLD7uWcaL._AC_SX466_.jpg'
+        },
+        {
+            'name': 'Scrabble',
+            'creator': 'Eureka',
+            'price': '150.00 PHP',
+            'description': 'A board-and-tile game in which two to four players compete in forming words with lettered tiles on a 225-square board.',
+            'url': 'https://5.imimg.com/data5/SELLER/Default/2021/7/DJ/JQ/WA/38245760/scrable-2-500x500.jpeg'
         }
     ],
-    'school supplies':[
+    'school supplies': [
         {
             'name': 'No. 2 Pencils',
-            'creator': 'Mongol',
-            'price': '150 PHP for 20 pcs',
-            'description': 'Easy writing with these reliable yellow pencils!'
+            'creator': 'Dixon',
+            'price': '150.00 PHP for 20 pcs.',
+            'description': 'Easy writing with these reliable yellow pencils!',
+            'url': 'https://cartoonresearch.com/wp-content/uploads/2022/04/pencills-1.jpg'
+        },
+        {
+            'name': 'Crayons',
+            'creator': 'Crayola',
+            'price': '50.00 PHP',
+            'description': 'This pack of 24 non-toxic crayons contains classic colors as well as bright, fun shades to inspire self-expression and imaginative play!',
+            'url': 'https://5.imimg.com/data5/TA/LM/MY-44916171/crayons-color-box-500x500.jpg'
+        },
+        {
+            'name': 'Bond Paper',
+            'creator': 'Hard Copy',
+            'price': '50.00 PHP for 100 pcs.',
+            'description': 'A high-quality durable writing paper that can be used for documentation, printing, drawing, writing, and many other activities.',
+            'url': 'https://ibitsphil.com/wp-content/uploads/2017/11/Hard-Copy-Copy-Paper.jpg'
         }
     ]
 }
-
 
